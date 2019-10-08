ec39ceb5a02860d27018616e3679cb47964550dd
artqz <djoctuk@yandex.ru>
2019-10-08 23:48:42 +0500

    Add Wario rep

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const conf = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true,
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/bode_modules/'
+      }
+    ]
+  }
+};
+
+module.exports = (env, options) => {
+  const production = options.mode === 'production';
+  conf.devtool = production ? 'source-map' : 'eval-sourcemap';
+
+  return conf;
+};
